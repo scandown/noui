997469345d4035b7b4a1d242719e1296e233c263
Josh Harris <yesmaileasy@gmail.com>
2026-08-10 23:40:35 +0100

    Initial commit
    Simple raylib example with text on the screen for the demo

---
+++ nob.c
@@ -0,0 +1,19 @@
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+
+
+#define BUILD_DIR "build/"
+
+int main(int argc, char **argv)
+{
+	nob_mkdir_if_not_exists(BUILD_DIR);
+	NOB_GO_REBUILD_URSELF(argc, argv);
+	Nob_Cmd cmd = {0};
+	nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", BUILD_DIR"main");
+	nob_cmd_append(&cmd, "src/demo/main.c");
+
+
+	nob_cmd_append(&cmd, "-lraylib");
+	if (!nob_cmd_run(&cmd)) return 1;
+	return 0;
+}
---
+++ src/demo/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <raylib.h>
+
+#define SCR_WIDTH 800
+#define SCR_HEIGHT 800
+
+int main() {
+
+	InitWindow(SCR_WIDTH, SCR_HEIGHT, "UI demo");
+
+
+	while (!WindowShouldClose()) {
+
+		ClearBackground(BLACK);
+
+		BeginDrawing();
+
+
+		char *word = "Hello, Sailour";
+
+		int font_height = 96;
+		int font_width = MeasureText(word, font_height);
+
+		DrawText(word, (SCR_WIDTH - font_width) / 2 , (SCR_HEIGHT - font_height)/2, font_height, WHITE);
+
+		EndDrawing();
+
+	}
+
+	printf("Hello, World\n");
+
+	CloseWindow();
+
+	return 0;
+}
